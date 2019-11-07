ea843a7f67aefd0e3a09ecced472d1b8dd6934ed
h3dx0 <rob3rto@openmailbox.org>
2019-11-06 21:57:16 -0600

    save doctor with calendar

--- hic/main/views.py
+++ hic/main/views.py
@@ -1,8 +1,8 @@
 import openpyxl
+import  json
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-
 
 # Create your views here.
 # @login_required
