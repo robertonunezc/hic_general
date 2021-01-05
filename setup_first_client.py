0b80973cbd30a4449c64a329a3500b09b3dcd971
Robert <rcorralesn@gmail.com>
2021-01-05 06:52:04 -0600

    add setup client script fix

--- setup_first_client.py
+++ setup_first_client.py
@@ -2,7 +2,7 @@ from customer.models import Client
 
 # create your public tenant
 tenant = Client(domain_url='hicsistema.herokuapp.com/', # don't add your port or www here! on a local server you'll want to use localhost here
-                schema_name='public',
+                schema_name='suc1',
                 name='Sucursal1',
                 plan='ANUAL',
                 started_date='2020-12-12'
