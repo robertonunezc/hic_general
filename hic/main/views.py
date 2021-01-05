2046ce434bea1ee6097806dbaa418dd2facb510f
Robert <rcorralesn@gmail.com>
2021-01-05 09:44:00 -0600

    add static config

--- hic/main/views.py
+++ hic/main/views.py
@@ -215,11 +215,5 @@ def first_tenant(request):
     from customer.models import Client
 
     # create your public tenant
-    tenant = Client(domain_url='hicsistema.herokuapp.com/',
-                    # don't add your port or www here! on a local server you'll want to use localhost here
-                    schema_name='suc1',
-                    name='Sucursal1',
-                    plan='ANUAL',
-                    started_date='2020-12-12'
-                    )
+    tenant = Client(domain_url='hic.puntoreica.com',schema_name='suc1',name='Sucursal1',plan='ANUAL',started_date='2020-12-12')
     tenant.save()
