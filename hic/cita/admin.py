f6d32cf5983fec449ff8d10f535f47df83a54178
Robert <rcorralesn@gmail.com>
2021-02-12 07:13:51 -0600

    Add ecita to admin

--- hic/cita/admin.py
+++ hic/cita/admin.py
@@ -6,6 +6,6 @@ from hic.cita.models import ECita, TCita, Calendario, Event, Cita, EventExtended
 admin.site.register(Calendario)
 # admin.site.register(Event)
 admin.site.register(Cita)
-# admin.site.register(ECita)
+admin.site.register(ECita)
 admin.site.register(TCita)
 # admin.site.register(EventExtendedProp)
